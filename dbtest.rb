1a13cbe812cb57405704fe975bbee9e8a6894ce9
longhai <longhaiqwe@126.com>
2021-08-06 08:58:56 +0800

    Part 8 - B-Tree Leaf Node Format(3)

--- dbtest.rb
+++ dbtest.rb
@@ -17,6 +17,25 @@ describe 'database' do
       raw_output.split("\n")
     end
 
+    it 'prints constants' do
+      script = [
+        ".constants",
+        ".exit",
+      ]
+      result = run_script(script)
+    
+      expect(result).to match_array([
+        "db > Constants:",
+        "ROW_SIZE: 293",
+        "COMMON_NODE_HEADER_SIZE: 6",
+        "LEAF_NODE_HEADER_SIZE: 10",
+        "LEAF_NODE_CELL_SIZE: 297",
+        "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+        "LEAF_NODE_MAX_CELLS: 13",
+        "db > ",
+      ])
+    end
+
     it 'inserts and retrieves a row' do
       result = run_script([
         "insert 1 user1 person1@example.com",
@@ -84,6 +103,27 @@ describe 'database' do
       "db > ID must be positive.",
       "db > Executed.",
       "db > ",
+    ])
+  end
+
+  it 'allows printing out the structure of a one-node btree' do
+    script = [3, 1, 2].map do |i|
+      "insert #{i} user#{i} person#{i}@example.com"
+    end
+    script << ".btree"
+    script << ".exit"
+    result = run_script(script)
+  
+    expect(result).to match_array([
+      "db > Executed.",
+      "db > Executed.",
+      "db > Executed.",
+      "db > Tree:",
+      "leaf (size 3)",
+      "  - 0 : 3",
+      "  - 1 : 1",
+      "  - 2 : 2",
+      "db > "
     ])
   end
 
--- main.c
+++ main.c
@@ -434,6 +434,15 @@ ExecuteResult execute_select(Statement *statement, Table *table) {
   return EXECUTE_SUCCESS;
 }
 
+void print_leaf_node(void *node) {
+  uint32_t num_cells = *leaf_node_num_cells(node);
+  printf("leaf (size %d)\n", num_cells);
+  for (uint32_t i = 0; i < num_cells; i++) {
+    uint32_t key = *leaf_node_key(node, i);
+    printf("  - %d : %d\n", i, key);
+  }
+}
+
 ExecuteResult execute_statment(Statement *statement, Table *table) {
   switch (statement->type) {
   case STATEMENT_INSERT:
@@ -443,10 +452,27 @@ ExecuteResult execute_statment(Statement *statement, Table *table) {
   }
 }
 
+void print_constants() {
+  printf("ROW_SIZE: %d\n", ROW_SIZE);
+  printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+  printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+  printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+  printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+  printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
 MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table) {
   if (strcmp(input_buffer->buffer, ".exit") == 0) {
     db_close(table);
     exit(EXIT_SUCCESS);
+  } else if (strcmp(input_buffer->buffer, ".constants") == 0) {
+    printf("Constants:\n");
+    print_constants();
+    return META_COMMAND_SUCCESS;
+  } else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+    printf("Tree:\n");
+    print_leaf_node(get_page(table->pager, 0));
+    return META_COMMAND_SUCCESS;
   } else {
     return META_COMMAND_UNRECOGNIZED_COMMAND;
   }
